a5a567d56496c70d1cdc157a98a9d81c13de6e16
S2G205ukai <simtight1108@gmail.com>
2024-11-09 15:56:10 +0900

    ゴールしたら白背景にCLEARを表示するのを実装した

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,6 +10,15 @@ public class Goal : MonoBehaviour
 
     [SerializeField] private Collider2D m_player;
     [SerializeField] private TextMeshProUGUI m_goalText;
+    [SerializeField] private CanvasGroup goalBackgroundImageCanvasGroup;
+    [SerializeField] private TextMeshProUGUI goalMeshProUGI;
+
+    private float m_timer;
+    private float fadeDuration = 1f;
+
+
+    private bool clearFLG = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +27,25 @@ public class Goal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (clearFLG)
+        {
+            GoalPerformance();
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        clearFLG = true;
         Debug.Log("test");
         m_goalText.SetText("Goal");
+
+    }
+
+    private void GoalPerformance()
+    {
+        m_timer += Time.deltaTime;
+        
+        goalBackgroundImageCanvasGroup.alpha = m_timer / fadeDuration;
+
     }
 }
