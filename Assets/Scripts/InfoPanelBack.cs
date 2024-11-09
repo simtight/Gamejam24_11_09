a6781097f3b09e0cfbb8db360cf199464393e4d6
S2G205ukai <simtight1108@gmail.com>
2024-11-10 01:06:31 +0900

    パネル関係を整えた

--- Assets/Scripts/InfoPanelBack.cs
+++ Assets/Scripts/InfoPanelBack.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class InfoPanelBack : MonoBehaviour
 {
     [SerializeField] private GameObject InfoPanel;
+    [SerializeField] private GameObject PausePanel;
 
     public void ShowPanel()
     {
+        Time.timeScale = 0.0f;
+        PausePanel.SetActive(false);
         InfoPanel.SetActive(true);
     }
 
     public void BackPanel()
     {
+        Time.timeScale = 1.0f;
         InfoPanel.SetActive(false);
     }
 }
