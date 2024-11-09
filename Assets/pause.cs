69c7303c501cb021a68b40393964d0d8452d8db6
SayanagiNayuta <nayuzi@icloud.com>
2024-11-09 17:30:04 +0900

    no message

---
+++ Assets/pause.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause : MonoBehaviour
+{
+    [SerializeField] GameObject panel;
+    public bool isPause = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel.SetActive(false);
+        isPause = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            if (isPause == false)
+            {
+                panel.SetActive(true);
+                Time.timeScale = 0f;
+                isPause = true;
+                Debug.Log(isPause);
+            }
+            else
+            {
+                panel.SetActive(false);
+                Time.timeScale = 1f;
+                isPause = false;
+                Debug.Log(isPause);
+            }
+        }
+    }
+}
