b2e834fa9705d1f7f0d925e3bc23a283dc709ebf
S2G205ukai <simtight1108@gmail.com>
2024-11-10 03:02:50 +0900

    スクリプト整えた

--- Assets/Scripts/PlayerBoxGrab.cs
+++ Assets/Scripts/PlayerBoxGrab.cs
@@ -11,6 +11,8 @@ public class PlayerBoxGrab : MonoBehaviour
     private bool canGrab = false;
     private bool isRight = false;
     private bool isLeft = false;
+    private bool isDown = false;
+    private bool isUp = false;
 
     private void Update()
     {
@@ -22,11 +24,24 @@ public class PlayerBoxGrab : MonoBehaviour
         {
             canGrab = false;
         }
-        if (Input.GetAxisRaw("Horizontal") >  0)
+        if (Input.GetAxisRaw("Horizontal") > 0)
         {
-            S
+            isLeft = true;
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            isRight = true;
+        }
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            isUp = true;
+        }
+        if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            isDown = true;
         }
     }
+        
 
     private void FixedUpdate()
     {
@@ -37,12 +52,12 @@ public class PlayerBoxGrab : MonoBehaviour
         else
         {
             
-            if ()
+            if (isRight)
             {
                 Debug.Log("test");
                 grabPoint.offset = new Vector2(this.transform.position.x + 1.0f, this.transform.position.y);
             }
-            else if(rb.velocity.x < 0)
+            else if(isLeft)
             {
                 grabPoint.offset = new Vector2(this.transform.position.x - 1.0f, this.transform.position.y);
             }
