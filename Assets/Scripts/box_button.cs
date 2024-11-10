9d49738d51bd5b8e2df623e942b25adb028b91ea
S2G205ukai <simtight1108@gmail.com>
2024-11-10 15:56:02 +0900

    BGM追加

--- Assets/Scripts/box_button.cs
+++ Assets/Scripts/box_button.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
-using UnityEditor.Experimental.RestService;
 using UnityEngine;
 
 public class box_button : MonoBehaviour
